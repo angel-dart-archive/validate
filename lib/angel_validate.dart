1e4e27f52e98fdff8884632181eeae26876e771b
thosakwe <thosakwe@gmail.com>
2016-12-27 11:59:21 -0500

    Scr

--- lib/angel_validate.dart
+++
@@ -1,6 +0,0 @@
-/// Cross-platform validation library based on `matcher`.
-library angel_validate;
-
-export 'package:matcher/matcher.dart';
-export 'src/matchers.dart';
-export 'src/validator.dart';
